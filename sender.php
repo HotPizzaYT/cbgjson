e941c90643764ccd2671620c6ca976f1fd3beda4
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2022-03-05 23:14:54 -0600

    Add raw key

--- sender.php
+++ sender.php
@@ -4,6 +4,9 @@ $jsond = json_decode($jsonf, true);
 $jsonnew = $jsond;
 if($_POST["m"] !== ""){
   $msgarr = array("user"=>$_POST["u"],"content"=>$_POST["m"],"type"=>"regular","time"=>time());
+  if(isset($_POST["rk"]) && $_POST["rk"] === "RawKeyWhateverYouWantBlahBlahBlah"){
+    $msgarr = array("user"=>$_POST["u"],"content"=>$_POST["m"],"type"=>"raw","time"=>time());
+  }
   array_unshift($jsonnew["messages"], $msgarr);
   $jsonne = json_encode($jsonnew);
   file_put_contents("chat.json", $jsonne);
